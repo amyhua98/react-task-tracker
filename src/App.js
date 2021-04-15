15e3d57e40ce4c8c0b780f3eb7150d13d83c2538
Amy Hua <aymay98@gmail.com>
2021-04-15 18:02:05 -0500

    Delete option

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import {useState} from 'react'
 
+const App = () =>{
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: '04/23/2021',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Party',
+      day: '04/24/2021',
+      reminder: true,
+    },
+  ])
 
-function App() {
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
-const Task = () => {
+const Task = ({task, onDelete}) => {
     return (
-        <div>
-            
+        <div className='task'>
+            <h3>
+                {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} 
+                onClick={() => onDelete()}/>
+            </h3>
+            <p>{task.day}</p>
         </div>
     )
 }
