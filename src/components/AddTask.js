20fcef7e5e59cbd619fae374dd51b6a6e587adf3
Amy Hua <aymay98@gmail.com>
2021-04-15 21:30:00 -0500

    Adding form to AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {useState} from 'react'
 
 const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
     return (
         <form className='add-form'>
-            
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add task'/>
+            </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type='text' placeholder='Add data and time'/>
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox'/>
+            </div>
+            <input className='btn btn-block' type='submit' value='Save Task' />
         </form>
     )
 }
