9ee68f2c66faa9778e42461dbbfe2cf33d26237e
Amy Hua <aymay98@gmail.com>
2021-04-17 14:49:24 -0500

    AddTask updated

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ const App = () =>{
       reminder: true,
     },
   ])
+  
+  //Add Task
+  const addTask = (task) => {
+    console.log(task)
+  }
 
   //Delete Task
   const deleteTask = (id) => {
@@ -34,7 +39,7 @@ const App = () =>{
   return (
     <div className='container'>
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? 
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
         : 'No Tasks to Show'}
