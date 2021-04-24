54ea1814bfc6a40b61ca620d3e867aeb0fcf06a5
Amy Hua <aymay98@gmail.com>
2021-04-24 10:52:19 -0500

    About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom';
 import Button from './Button'
 
 const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'tan' : 'green'} 
+            {location.pathname === '/' && <Button 
+            color={showAdd ? 'tan' : 'green'} 
             text={showAdd ? 'Close' : 'Add'} 
-            onClick={onAdd}/>
+            onClick={onAdd}
+            />}
         </header>
     )
 }
