3d7eb5732dee40e8b7253c55f7ef1ae8e73677a0
Amy Hua <aymay98@gmail.com>
2021-05-05 12:38:12 -0500

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({title, onAdd, showAdd}) => {
         <header className='header'>
             <h1>{title}</h1>
             {location.pathname === '/' && <Button 
-            color={showAdd ? 'tan' : 'green'} 
+            color={showAdd ? 'tan' : 'navy'} 
             text={showAdd ? 'Close' : 'Add'} 
             onClick={onAdd}
             />}
